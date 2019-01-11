3244002cbf9601a9f5fa0f7023b3918f87f7480e
antsteyer <antoinesteyer@gmail.com>
2019-01-11 15:35:41 +0100

    Add vue-socket.io to project

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import ElementUI from 'element-ui';
+import VueSocketIO from 'vue-socket.io';
 import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
+Vue.use(
+  new VueSocketIO({
+    debug: true,
+    connection: '10.212.111.122:3000',
+  }),
+);
 new Vue({
   router,
   render: (h) => h(App),
